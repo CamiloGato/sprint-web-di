02229df8735e3b59c9c99f4ffeb2c7903fbae01b
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-07-12 15:47:10 -0500

    Inicio Proyecto

---
+++ src/main/java/models/service/MiServicio.java
@@ -0,0 +1,7 @@
+package models.service;
+
+public class MiServicio {
+    public String operacion(){
+        return "Ejecutando algún proceso";
+    }
+}
---
+++ src/test/java/com/example/sprintwebdi/SprintWebDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintwebdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintWebDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
